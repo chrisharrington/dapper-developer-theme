d9d98e9c31f2e63db60e112e691beed1088aae4e
Chris Harrington <chrisharrington99@gmail.com>
2014-12-25 22:09:44 -0700

    Added post list with author info, publish date and tags.

--- index.php
+++ index.php
@@ -9,14 +9,22 @@
         <div class="container-fluid header">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-4 col-sm-push-8">
-                        <div>
-                            <i class="fa fa-twitter"></i>
-                        </div>    
-                    </div>
-                    <div class="col-sm-8 col-sm-pull-4">
+                    <div class="col-sm-8">
                         <h1><?php echo get_bloginfo('name'); ?></h1>
                         <h5><?php echo get_bloginfo('description'); ?></h5>
+                    </div>
+                    <div class="col-sm-4 visible-sm visible-md visible-lg">
+                        <div class="pull-right">
+                            <a href="https://twitter.com/charrington99" target="_blank">
+                                <i class="fa fa-twitter-square"></i>
+                            </a>
+                            <a href="https://plus.google.com/+ChrisHarrington" target="_blank">
+                                <i class="fa fa-google-plus-square"></i>
+                            </a>
+                            <a href="https://ca.linkedin.com/in/chrisharrington99" target="_blank">
+                                <i class="fa fa-linkedin-square"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,7 +33,34 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-9">
-                    the article
+                    <?php
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post(); ?>
+
+                            <div class="post">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        By <?php the_author(); ?> on <?php the_date(); ?>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <?php the_tags('<i class="fa fa-tags"></i>'); ?>
+                                    </div>
+                                </div>
+                                
+                                <div class="info">
+                                    
+                                    <span class="tags">
+                                        
+                                    </span>
+                                </div>
+                                <h2><?php the_title(); ?></h2>
+                                <?php the_excerpt(); ?>
+                            </div>
+                    
+                        <?php }
+                    }
+                    ?>
                 </div>
                 
                 <div class="col-sm-3">
