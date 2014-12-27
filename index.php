0c41f9f97adcc2c40a8bf1b84b15a1b1c02d7af0
Chris Harrington <chrisharrington99@gmail.com>
2014-12-27 11:32:17 -0700

    Post information now hidden for pages.

--- index.php
+++ index.php
@@ -10,20 +10,42 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-8">
-                        <h1><?php echo get_bloginfo('name'); ?></h1>
-                        <h5><?php echo get_bloginfo('description'); ?></h5>
+                        <a href="index.php" class="logo">
+                            <h1><?php echo get_bloginfo('name'); ?></h1>
+                            <h5><?php echo get_bloginfo('description'); ?></h5>
+                        </a>
                     </div>
                     <div class="col-sm-4 visible-sm visible-md visible-lg">
                         <div class="pull-right">
-                            <a href="https://twitter.com/charrington99" target="_blank">
-                                <i class="fa fa-twitter-square"></i>
-                            </a>
-                            <a href="https://plus.google.com/+ChrisHarrington" target="_blank">
-                                <i class="fa fa-google-plus-square"></i>
-                            </a>
-                            <a href="https://ca.linkedin.com/in/chrisharrington99" target="_blank">
-                                <i class="fa fa-linkedin-square"></i>
-                            </a>
+                            <div class="row">
+                                <div class="col-sm-12 text-right">
+                                    <a href="https://github.com/chrisharrington" target="_blank">
+                                        <i class="fa fa-github-square"></i>
+                                    </a>
+                                    <a href="https://twitter.com/charrington99" target="_blank">
+                                        <i class="fa fa-twitter-square"></i>
+                                    </a>
+                                    <a href="https://plus.google.com/+ChrisHarrington" target="_blank">
+                                        <i class="fa fa-google-plus-square"></i>
+                                    </a>
+                                    <a href="https://ca.linkedin.com/in/chrisharrington99" target="_blank">
+                                        <i class="fa fa-linkedin-square"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <ul class="pages pull-right">
+                                        <?php
+                                        $html = "";
+                                        foreach (get_pages() as $page) {
+                                            $html .= "<li><a href='" . get_page_link($page->ID) . "'>" . $page->post_title . "</a></li>";
+                                        }
+                                        echo $html;
+                                        ?>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -39,6 +61,7 @@
                             the_post(); ?>
 
                             <div class="post">
+                                <?php if (get_post_type() == "post"): ?>
                                 <div class="row info">
                                     <div class="col-sm-6">
                                         Posted on <?php the_date(); ?> by <?php the_author(); ?>
@@ -53,7 +76,7 @@
                                             $tags = get_the_tags();
                                             if ($tags) {
                                                 $html = "<div class='tags pull-left'><i class='fa fa-tags'></i>";
-                                                foreach (get_the_tags() as $tag) {
+                                                foreach ($tags as $tag) {
                                                     $tag_link = get_tag_link($tag->term_id);
                                                     $html .= "<a href='{$tag_link}'>";
                                                     $html .= "{$tag->name}</a>, ";
@@ -61,12 +84,14 @@
                                                 $html = substr($html, 0, strlen($html)-2);
                                                 $html .= '</div>';
                                                 echo $html;
-                                            }
+                                            }   
                                             ?>
                                         </div>
                                     </div>
                                 </div>
-                                <h2><?php the_title(); ?></h2>
+                                <?php endif; ?>
+                                
+                                <a href="index.php?p=<?php the_ID(); ?>"><h1><?php the_title(); ?></h1></a>
                                 <?php the_content("Continue reading <i class='fa fa-long-arrow-right'></i>"); ?>
                             </div>
                     
