08165e4ea9c7959cca8e87e0ab2624e548e08f5c
Chris Harrington <chrisharrington99@gmail.com>
2014-12-30 11:59:24 -0700

    Added bower and grunt tasks to compile bower components.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,27 @@ module.exports = function(grunt) {
     require("jit-grunt")(grunt);
 
     grunt.initConfig({
+        bowercopy: {
+            options: {
+                srcPrefix: "bower_components"
+            },
+            scripts: {
+                options: {
+                    destPrefix: "script/vendor"
+                },
+                files: {
+                    "require.js": "require/build/require.js",
+                    "github-api.js": "github-api/github.js"
+                }
+            }
+        },
+        
+        concat: {
+            dist: {
+                src: ["script/vendor/**/*.js", "script/app/**/*.js"],
+                dest: "app.js"
+            }
+        },
         less: {
             development: {
                files: {
@@ -10,15 +31,23 @@ module.exports = function(grunt) {
             }
         },
         watch: {
+            scripts: {
+                files: ["script/app/**/*.js"],
+                tasks: ["concat"],
+                options: {
+                    spawn: false
+                }
+            },
+            
             styles: {
                 files: ["style/**/*.less", "style/**/*.css"],
                 tasks: ["less"],
                 options: {
-                    nospawn: true
+                    spawn: false
                 }
             }
         }
     });
 
-    grunt.registerTask("default", ["less", "watch"]);
+    grunt.registerTask("default", ["bowercopy", "concat", "less", "watch"]);
 };
