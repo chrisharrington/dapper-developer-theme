daf2284a6b241f7517b8c0154b5bc6bebd3dc407
Chris Harrington <chrisharrington99@gmail.com>
2015-01-02 21:48:35 -0700

    Added more/less button for github repos.

--- script/app/components/github/index.js
+++ script/app/components/github/index.js
@@ -7,7 +7,8 @@ var React = require("react"),
 module.exports = React.createClass({
     getInitialState: function() {
         return {
-            repos: []
+            repos: [],
+            moreVisible: false
         };
     },
     
@@ -16,12 +17,23 @@ module.exports = React.createClass({
         github.repos().then(function(repos) {
             this.setState({ repos: repos });
         }.bind(this));
-    },        
+    },
     
-    renderRepos: function() {
-        var counter = 0;
-        return _.map(this.state.repos, function(repo) {
-            return <a href={repo.url} target="_blank" key={repo.id} className="row hover-link block">
+    toggleMore: function() {
+        this.setState({ moreVisible: !this.state.moreVisible });  
+    },
+    
+    renderRepos: function(start, end) {
+        if (this.state.repos.length === 0)
+            return "";
+        
+        var counter = start;
+        end = end || this.state.repos.length-1;
+        
+        var repos = [];
+        for (var i = start; i < end; i++) {
+            var repo = this.state.repos[i];
+            repos.push(<a href={repo.url} target="_blank" key={repo.id} className="row hover-link block">
                 <div className="col-md-12 padding-top-5 padding-bottom-5">
                     <div className="row">
                         <div className="col-md-12 bold">{repo.name}</div>
@@ -36,14 +48,23 @@ module.exports = React.createClass({
                         </div>
                     </div>
                 </div>
-            </a>;
-        });
+            </a>);
+        }
+        return repos;
     },
     
     render: function() {
-        return <div className="github padding-15 small-font box-sizing pull-left full-width">
+        return <div className="github padding-15 box-sizing pull-left full-width">
             <h3 className="upper-case spacing-bottom-10">GitHub</h3>
-            {this.renderRepos()}
+            {this.renderRepos(0, 5)}
+            <div className={"more " + (this.state.moreVisible ? "" : "hidden")}>
+                {this.renderRepos(6)}
+            </div>
+            <div className="row">
+                <div className="col-md-12 text-right spacing-top-10">
+                    <a onClick={this.toggleMore}>{this.state.moreVisible ? "Less" : "More"}...</a>
+                </div>
+            </div>
         </div>;   
     }
 });
--- script/app/config.js
+++ script/app/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     serviceLocation: "http://130.211.172.19:8081/"
+    //serviceLocation: "http://localhost:8081/"
 };
