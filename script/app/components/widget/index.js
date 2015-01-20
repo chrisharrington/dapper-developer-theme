eab368d0c65d82ef84eb883d13c2118c67bdb49c
Chris Harrington <chrisharrington99@gmail.com>
2015-01-20 15:31:29 -0700

    Tweaked spacing between widgets.

--- script/app/components/widget/index.js
+++ script/app/components/widget/index.js
@@ -2,7 +2,7 @@ var React = require("react");
 
 module.exports = React.createClass({
     render: function() {
-        return <div className={"small-font " + this.props.className + " " + (this.props.hidden ? " hidden" : "")}>
+        return <div className={"small-font spacing-top-10 " + (this.props.className || "") + " " + (this.props.hidden ? " hidden" : "")}>
             <h3 className="upper-case spacing-bottom-10">{this.props.title}</h3>
             {this.props.children}
         </div>;
