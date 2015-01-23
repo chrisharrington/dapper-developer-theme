c36b7e4fef0c76f7914d2066d22ed6776035526e
Chris Harrington <chrisharrington99@gmail.com>
2015-01-23 14:23:41 -0700

    Restored twitter widget. Whoops.

---
+++ script/app/components/twitter/index.js
@@ -0,0 +1,14 @@
+var React = require("react"),
+Twitter = require("data/twitter"),
+
+Widget = require("components/widget");
+
+config = require("config");
+
+module.exports = React.createClass({
+    render: function() {
+        return <Widget title="Twitter" className="twitter">
+            <a ref="link" className="twitter-timeline" href="https://twitter.com/charrington99" data-widget-id="544598743610175488" data-tweet-limit="5">Tweets by @charrington99</a>
+        </Widget>;
+    }
+});
